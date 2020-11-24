5473411c0da5d2ddc1f9d1765d1239e373cb4058
Andres Reyes <ar97yf@gmail.com>
2020-11-24 18:46:39 -0500

    [ADD] Payment and Success

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import '../styles/components/Success.css';
 
 const Success = () => {
-  return <h1>Success</h1>;
+  return (
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Andres, Gracias por tu compra</h2>
+        <span>Tu pedido llegara en tres dias a tu dirección:</span>
+        <div className="Success-map">Google Maps</div>
+      </div>
+    </div>
+  );
 };
 
 export default Success;
