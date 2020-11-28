f2d54dbf7c870893958a5ad28592f9972179fde8
Andres Reyes <ar97yf@gmail.com>
2020-11-27 21:31:57 -0500

    [ADD] Helmet config

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Products from '../components/Products';
 import initialState from '../initialState';
 
 const Home = () => {
-  return <Products products={initialState.products} />;
+  <>
+    <Helmet>
+      <title>Platzi Conf Merch - Productos</title>
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@areyesdev" />
+      <meta name="twitter:creator" content="@areyesdev" />
+      <meta name="twitter:title" content="Platzi Conf Store" />
+      <meta name="twitter:description" content="Platzi Conf Store" />
+      <meta name="twitter:image" content="" />
+      <meta property="og:title" content="Platzi Conf Store" />
+      <meta property="og:description" content="Platzi Conf Store" />
+      <meta property="og:image" content="" />
+      <meta property="og:url" content="platzistore.xyz" />
+      <meta property="og:site_name" content="Platzi Conf Store" />
+      <meta property="og:locale" content="es_ES" />
+      <meta property="og:type" content="article" />
+      <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
+    </Helmet>
+    <Products products={initialState.products} />;
+  </>;
 };
 
 export default Home;
